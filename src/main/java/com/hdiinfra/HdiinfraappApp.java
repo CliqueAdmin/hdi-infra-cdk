c3be2cdcf8d9e25d9c4d80788f4d0769fb33d805
Clique Admin <Cliquedmn@gmail.com>
2021-10-23 23:30:23 -0700

    Add git pipeline

---
+++ src/main/java/com/hdiinfra/HdiinfraappApp.java
@@ -0,0 +1,22 @@
+package com.hdiinfra;
+
+import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.StackProps;
+
+import java.util.Arrays;
+
+public class HdiinfraappApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        new HdiinfraappStack(app, "HdiinfraappStack", StackProps.builder()
+            .env( Environment.builder()
+                .account("102733166919")
+                .region("us-west-2")
+                .build())
+            .build());
+
+        app.synth();
+    }
+}
---
+++ src/main/java/com/hdiinfra/HdiinfraappStack.java
@@ -0,0 +1,28 @@
+package com.hdiinfra;
+
+import java.util.Arrays;
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.pipelines.CodePipeline;
+import software.amazon.awscdk.pipelines.CodePipelineSource;
+import software.amazon.awscdk.pipelines.ShellStep;
+
+public class HdiinfraappStack extends Stack {
+    public HdiinfraappStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public HdiinfraappStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+        CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
+            .pipelineName("MyPipeline")
+            .synth(ShellStep.Builder.create("Synth")
+                .input(CodePipelineSource.gitHub("CliqueAdmin/hdi-infra-cdk", "main"))
+                .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
+                .build())
+            .build();
+    }
+
+
+}
