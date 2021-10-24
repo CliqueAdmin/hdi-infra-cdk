a5a3fbbfd678525f25860df991e3f53cea91391e
Clique Admin <Cliquedmn@gmail.com>
2021-10-24 00:05:00 -0700

    Correct the pipeline name

--- src/main/java/com/hdiinfra/HdiinfraappStack.java
+++ src/main/java/com/hdiinfra/HdiinfraappStack.java
@@ -7,6 +7,7 @@ import software.amazon.awscdk.core.StackProps;
 import software.amazon.awscdk.pipelines.CodePipeline;
 import software.amazon.awscdk.pipelines.CodePipelineSource;
 import software.amazon.awscdk.pipelines.ShellStep;
+import software.amazon.awscdk.services.ecr.Repository;
 
 public class HdiinfraappStack extends Stack {
     public HdiinfraappStack(final Construct scope, final String id) {
@@ -16,12 +17,14 @@ public class HdiinfraappStack extends Stack {
     public HdiinfraappStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
         CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
-            .pipelineName("MyPipeline")
+            .pipelineName("HdiInfraCICD")
             .synth(ShellStep.Builder.create("Synth")
                 .input(CodePipelineSource.gitHub("CliqueAdmin/hdi-infra-cdk", "main"))
                 .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
                 .build())
             .build();
+
+//        Repository.Builder.create(scope, id+"-ecr").repositoryName("")
     }
 
 
