291db98c17f783e89f1c45feccd425fe80a18667
Clique Admin <Cliquedmn@gmail.com>
2021-10-24 00:11:03 -0700

    Add repository for hdi apps

--- src/main/java/com/hdiinfra/HdiinfraappStack.java
+++ src/main/java/com/hdiinfra/HdiinfraappStack.java
@@ -10,22 +10,23 @@ import software.amazon.awscdk.pipelines.ShellStep;
 import software.amazon.awscdk.services.ecr.Repository;
 
 public class HdiinfraappStack extends Stack {
-    public HdiinfraappStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
 
-    public HdiinfraappStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
-        CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
-            .pipelineName("HdiInfraCICD")
-            .synth(ShellStep.Builder.create("Synth")
-                .input(CodePipelineSource.gitHub("CliqueAdmin/hdi-infra-cdk", "main"))
-                .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
-                .build())
-            .build();
+  public HdiinfraappStack(final Construct scope, final String id) {
+    this(scope, id, null);
+  }
 
-//        Repository.Builder.create(scope, id+"-ecr").repositoryName("")
-    }
+  public HdiinfraappStack(final Construct scope, final String id, final StackProps props) {
+    super(scope, id, props);
+    CodePipeline pipeline = CodePipeline.Builder.create(this, "pipeline")
+        .pipelineName("HdiInfraCICD")
+        .synth(ShellStep.Builder.create("Synth")
+            .input(CodePipelineSource.gitHub("CliqueAdmin/hdi-infra-cdk", "main"))
+            .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
+            .build())
+        .build();
+
+    Repository.Builder.create(scope, id + "-ecr").repositoryName("hdirepos-cicd").build();
+  }
 
 
 }
